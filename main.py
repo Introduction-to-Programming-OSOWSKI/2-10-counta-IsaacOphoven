f7f69dce5602ec74b919665f69526768e0131946
isaacophoven <824067@mystma.org>
2021-10-06 09:36:16 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 #WRITE YOUR CODE IN THIS FILE
+#ok
+
+def countA(w):
+
+    Bub = 0
+
+    for i in range(0, len(w)):
+        if w[i] == "a":
+            Bub = Bub + 1
+    return Bub
+
+    
+print(countA("armadilloo"))
